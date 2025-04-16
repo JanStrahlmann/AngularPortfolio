97223ca3adcb299363d2d86ffb390aafac3b4b43
Jan Strahlmann <janstrahlmann@gmail.com.com>
2025-04-16 21:56:51 +0200

    Kontaktseite fertig

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -11,5 +12,20 @@ export class ContactComponent {
 
   constructor(private titleService: Title) {
     this.titleService.setTitle('Jan Strahlmann - Kontakt')
+  }
+
+  showCopiedPopup = false;
+
+  copyToClipboard(text: string): void {
+    navigator.clipboard.writeText(text).then(() => {
+      this.showCopiedPopup = true;
+
+      // Blende das Popup nach 2 Sekunden wieder aus
+      setTimeout(() => {
+        this.showCopiedPopup = false;
+      }, 2000);
+    }).catch(err => {
+      console.error('Fehler beim Kopieren:', err);
+    });
   }
 }
