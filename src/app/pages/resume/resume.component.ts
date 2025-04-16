7a7a8e88144f39b0885f8b84518d4995ed03738f
Jan Strahlmann <janstrahlmann@gmail.com.com>
2025-04-16 19:20:04 +0200

    new resume

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,14 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { AccordionComponent, AccordionPanelComponent } from 'ngx-bootstrap/accordion';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-resume',
+  imports: [
+    CommonModule,
+    AccordionComponent,
+    AccordionPanelComponent
+  ],
   templateUrl: './resume.component.html',
   styleUrl: './resume.component.scss'
 })
 export class ResumeComponent {
 
-  constructor(private titleService: Title) {
+
+  isWorkExperienceOpen: boolean = false;
+  isTraineeOpen: boolean = false;
+  isSkillsOpen: boolean = false;
+
+  constructor(private titleService: Title, private renderer: Renderer2) {
     this.titleService.setTitle('Jan Strahlmann - Resume')
+  }
+
+  DownloadFile() {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', 'Resume.pdf');
+    link.setAttribute('download', 'Resume.pdf');
+    link.click();
+    link.remove();
   }
 }
